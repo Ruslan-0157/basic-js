ca474be8236c4eef76c279c9c5afe3a7f7000619
Ruslan-0157 <70931398+Ruslan-0157@users.noreply.github.com>
2020-11-01 21:46:46 +0100

    feat: add function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const repeatTimes =
+    options.repeatTimes === undefined ? 1 : options.repeatTimes;
+  const separator = options.separator === undefined ? "+" : options.separator;
+  let addition =
+    options.addition === undefined
+      ? ""
+      : options.addition === null
+      ? `${options.addition}`
+      : options.addition.toString();
+  const additionRepeatTimes =
+    options.additionRepeatTimes !== undefined ? options.additionRepeatTimes : 1;
+  const additionSeparator =
+    options.additionSeparator !== undefined ? options.additionSeparator : "|";
+  const initStr = str === null ? `${str}` : str.toString();
+  let result = "";
+  let resultAddition = "";
+  if (addition !== "") {
+    for (i = 0; i < additionRepeatTimes; i++) {
+      if (i + 1 === additionRepeatTimes) resultAddition += addition;
+      else resultAddition += addition + additionSeparator;
+    }
+  }
+  for (i = 0; i < repeatTimes; i++) {
+    if (i + 1 === repeatTimes) result += initStr + resultAddition;
+    else result += initStr + resultAddition + separator;
+  }
+
+  return result;
 };
   
