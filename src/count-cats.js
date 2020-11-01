b637631e1b1be205ab87a229ee9fae92dfc67125
Ruslan-0157 <70931398+Ruslan-0157@users.noreply.github.com>
2020-11-01 21:44:56 +0100

    feat: add function count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let counter = 0;
+  matrix.forEach((massiv) => {
+    const foundCats = massiv.filter((el) => el === "^^");
+    counter += foundCats.length;
+  });
+  return counter;
 };
+
